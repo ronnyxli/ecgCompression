759146fd95de20d4670b84b384e2af6582642398
Ronny Li <ronnyxli@gmail.com>
2018-08-20 10:11:37 -0700

    move plotting to main.m

--- compress_ecg.m
+++ compress_ecg.m
@@ -7,15 +7,7 @@ ENERGY_THRESH = 0.90;
 y_zm = y - mean(y);
 y_norm = y_zm/max(abs(y_zm));
 
-figure;
-subplot(3,1,1);
-plot(y_norm);
-grid on;
-hold on;
-title('Normalized ECG signal');
-xlabel('Sample #');
-
-% DCT of ECG return coefficients
+% DCT of ECG - return coefficients
 y_dct = dct(y_norm);
 
 % determine coefficients that make up ENERGY_THRESH% of energy
@@ -23,7 +15,7 @@ y_dct_cum = cumsum(abs(y_dct));
 energy_thresh = y_dct_cum(end)*ENERGY_THRESH;
 y_dct_selected = y_dct(y_dct_cum < energy_thresh);
 
-% scale coefficients by minimum
+% scale coefficients by minimum to get all positive numbers
 dct_shift = max(abs(y_dct_selected));
 y_dct_scaled = y_dct_selected + dct_shift;
 
@@ -31,30 +23,16 @@ y_dct_scaled = y_dct_selected + dct_shift;
 
 y_dct_final = y_dct_scaled;
 
-% compression ratio
-CR = length(y_norm)/length(y_dct_final);
-
-subplot(3,1,2);
-stem(y_dct_final, '-o');
-grid on;
-title('Discrete cosine transform coefficients of original ECG');
-xlabel('Sample #');
-
 % zero-padding
 y_dct_recon = y_dct_final - dct_shift;
 pad_vec = zeros(length(y) - length(y_dct_final),1);
 y_dct_new = [y_dct_recon; pad_vec];
 
-% inverse DCT
-y_new = idct(y_dct_new);
-
-% plot reconstructed signal with original
-subplot(3,1,3);
-title(['Compression Ratio = ' num2str(CR) ':1']);
-plot(y_norm); hold on;
-plot(y_new);
-grid on;
-legend('Original','Reconstructed');
+% inverse DCT (reconstructed signal)
+out.y_new = idct(y_dct_new);
 
 out.dct_coeff = y_dct_final;
+out.dct_shift = dct_shift;
+
+out.y_norm = y_norm;
 
--- main.m
+++ main.m
@@ -6,5 +6,34 @@ load('mitdb_100.mat');
 
 out = compress_ecg(t, signal(:,1), Fs);
 
+% calculate compression ratio
+CR = length(signal(:,1))/length(out.dct_coeff);
+
+% plot results
+
+figure;
+subplot(3,1,1);
+plot(out.y_norm);
+grid on;
+hold on;
+title('Normalized ECG signal');
+xlabel('Sample #');
+
+subplot(3,1,2);
+stem(out.dct_coeff, '-o');
+grid on;
+title('Discrete cosine transform coefficients of original ECG');
+xlabel('Sample #');
+
+% plot reconstructed signal with original
+subplot(3,1,3);
+title(['Compression Ratio = ' num2str(CR) ':1']);
+plot(out.y_norm); hold on;
+plot(out.y_new);
+grid on;
+legend('Original','Reconstructed');
+
+
+
 % TODO: calculate RMSE
 
